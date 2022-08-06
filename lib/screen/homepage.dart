06b1c24922eab87fb06efaca3b796a778cbaddc0
Suban Shrestha <076bct082.suban@pcampus.edu.np>
2022-08-06 09:13:58 +0545

    First db query

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,15 +1,51 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class sathi_user {
+  String name = "";
+  String email = "";
+  bool type = false;
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var name = "Suban";
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    if (FirebaseAuth.instance.currentUser != null) {
+      print(FirebaseAuth.instance.currentUser?.uid);
+    }
+
     return Scaffold(
       body: Column(
         children: [
-          Text('this is me'),
+          FutureBuilder<DocumentSnapshot>(
+            future: users.doc(user!.uid).get(),
+            builder: ((context, snapshot) {
+              if (snapshot.hasError) {
+                return Text("has error");
+              }
+              if (snapshot.hasData && !snapshot.data!.exists) {
+                return Text("Document does not exist");
+              }
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, dynamic> data =
+                    snapshot.data!.data() as Map<String, dynamic>;
+                return Text("Full Name: ${data['name']} type: ${data['type'] == 'true'? "1": "2"}");
+              }
+              return Text('loading');
+            }),
+          ),
           TextButton(
               onPressed: () {
                 FirebaseAuth.instance.signOut();
